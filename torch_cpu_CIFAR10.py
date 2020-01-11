d1d327fcb162a7d22dbe4497aa0b380c8debc80f
Aashish Gupta <51059360+ash81197@users.noreply.github.com>
2020-01-11 18:09:18 +0530

    Update torch_cpu_CIFAR10.py

--- torch_cpu_CIFAR10.py
+++ torch_cpu_CIFAR10.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 
 batch_size = 256
 
-# Creating Class
+# Class
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
@@ -15,30 +15,29 @@ class LeNet(nn.Module):
                                         nn.AvgPool2d(2, stride = 2),
                                         nn.Conv2d(6, 16, 5),
                                         nn.Tanh(),
-                                        nn.AvgPool2d(2, stride = 2),
-                                        )
+                                        nn.AvgPool2d(2, stride = 2))
+        
         self.dense_model = nn.Sequential(nn.Linear(400, 120),
                                          nn.Tanh(),
                                          nn.Linear(120, 84),
                                          nn.Tanh(),
-                                         nn.Linear(84, 10)
-                                         )
-    # Feed Forward Method
+                                         nn.Linear(84, 10))
     def forward(self, x):
-        #x = x.to(device)
         y = self.conv_model(x)
         # Flatten the result from conv model
         y = torch.flatten(y, 1)
         y = self.dense_model(y)
         return y
 
+# Training set
 trainset = torchvision.datasets.CIFAR10(root = './data',
                                         train = True,
                                         download = True,
                                         transform = torchvision.transforms.ToTensor())
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size = batch, shuffle = True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size = batch_size, shuffle = True)
 
+# Testing set
 testset = torchvision.datasets.CIFAR10(root = './data',
                                        train = False,
                                        download = True,
@@ -50,12 +49,12 @@ net = LeNet()
 loss_func = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters())
 
+# Evaluating Model Performance
 def model_evalutaion(dataloader):
     total = 0
     correct = 0
     for data in dataloader:
         image_data, labels = data
-        #image_data = data[0].to(device), data[1].to(device)
         out = net(image_data)
         max_values, pred_class = torch.max(out, dim = 1)
         total += labels.shape[0]
@@ -75,6 +74,6 @@ for i in range(total_epochs):
         optimizer.step()
     train_acc = model_evalutaion(trainloader)
     test_acc = model_evalutaion(testloader)
-    print("Epoch:", i, ", Test Accuracy:", test_acc, ", Train Accuracy:", train_acc)
+    print("Epoch:", i+1, ", Test Accuracy:", test_acc, ", Train Accuracy:", train_acc)
 end = time.perf_counter()
 print("Time taken to execute the program is: ", end-start)
